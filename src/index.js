2592f11d21b07b26a3acc4f2be80f91d6f0287c2
Robin Green <robin@designingreen.co.uk>
2020-04-17 11:55:38 +0100

    Update mailchimp code

--- src/index.js
+++ src/index.js
@@ -5,15 +5,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById(‘root’));
-
-/* ReactDOM.render(
+ReactDOM.render(
   <AppProviders>
     <App />
   </AppProviders>,
   document.getElementById('root'));
-*/
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
