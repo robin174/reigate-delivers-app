01da251d764798677076a79d1cae02e21833bacd
Robin Green <robin@designingreen.co.uk>
2020-05-22 12:43:11 +0100

    Re-established variables

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { CustomForm } from './components/Forms';
 const LocationList = React.lazy(() => import('./components/Locations').then(module => ({ default: module.LocationList })));
 const Modal = React.lazy(() => import('./components/Modal').then(module => ({ default: module.Modal })));
 
-const url = {process.env.REACT_APP_MAILCHIMP_URL}; 
+const url = "https://reigatedelivers.us4.list-manage.com/subscribe/post?u=ef1ec5977a149466897cdbb1a&amp;id=ea0294d02e"
 
 const App = () => {
   return (
