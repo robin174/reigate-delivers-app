4cce4c24b112542dcdc376fb4c8d5b2806a596f7
Robin Green <robin@designingreen.co.uk>
2020-05-22 12:01:53 +0100

    Updated graphics

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import { HeroHeader, Nav } from './components/Headers';
 import { LoadingSpinner } from './components/Helpers';
 import { CustomForm } from './components/Forms';
 
+console.log(process.env.REACT_APP_CLOUDINARY_CLOUD_NAME)
 
 // Code Splitting
 const LocationList = React.lazy(() => import('./components/Locations').then(module => ({ default: module.LocationList })));
 const Modal = React.lazy(() => import('./components/Modal').then(module => ({ default: module.Modal })));
 
-const url = 'https://reigatedelivers.us4.list-manage.com/subscribe/post?u=ef1ec5977a149466897cdbb1a&amp;id=ea0294d02e';
+const url = 'process.env.REACT_APP_MAILCHIMP_URL';
 
 const App = () => {
   return (
